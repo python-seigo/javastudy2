217ed8831c798499a4009b1ebe9239a70eeb6278
gitnissy <zhengwuxichuan@gmail.com>
2024-03-23 22:36:25 +0900

    val change

--- src/test/java/com/example/demo/StudyspringgitApplicationTests.java
+++ src/test/java/com/example/demo/StudyspringgitApplicationTests.java
@@ -1,7 +1,5 @@
 package com.example.demo;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,7 +8,7 @@ class StudyspringgitApplicationTests {
 
 	@Test
 	void contextLoads() {
-		assertEquals(1, 0);
+		
 	}
 
 }
