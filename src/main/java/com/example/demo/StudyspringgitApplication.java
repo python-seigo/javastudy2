1307ba92c1e35d6ff5a02d2b120860b8012f92e3
gitnissy <zhengwuxichuan@gmail.com>
2024-03-23 21:44:35 +0900

    add project

---
+++ src/main/java/com/example/demo/StudyspringgitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyspringgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyspringgitApplication.class, args);
+	}
+
+}
